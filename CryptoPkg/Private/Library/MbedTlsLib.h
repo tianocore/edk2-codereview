tianocore/edk2

CryptoPkg: Add Mbedtls submodule in CI
Change CI for new Mbedtls submodule.

REF: https://bugzilla.tianocore.org/show_bug.cgi?id=4177

Cc: Jiewen Yao <jiewen.yao@intel.com>
Cc: Yi Li <yi1.li@intel.com>
Cc: Xiaoyu Lu <xiaoyu1.lu@intel.com>
Cc: Guomin Jiang <guomin.jiang@intel.com>
Signed-off-by: Wenxing Hou <wenxing.hou@intel.com>

CryptoPkg/Private/Library/MbedTlsLib.h
@@ -1,0 +1,12 @@
+/** @file
+  OpensslLib class with APIs from the openssl project
+
+  Copyright (c) 2023, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef MBEDTLS_LIB_H_
+#define MBEDTLS_LIB_H_
+
+#endif

tianocore/edk2-codereview

Reviewed-by: Yi Li <yi1.li@intel.com>
Reviewed-by: Michael D Kinney <michael.d.kinney@intel.com>

CryptoPkg/Private/Library/MbedTlsLib.h
@@ -1,0 +1,12 @@
+/** @file
+  MbedTlsLib class with APIs from the mbedtls project
+
+  Copyright (c) 2023, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef MBEDTLS_LIB_H_
+#define MBEDTLS_LIB_H_
+
+#endif